216222efa8eb39a5783ab305091cdaa28e005c2a
lmac-1 <macartney.lucy@gmail.com>
2022-02-09 21:54:53 -0500

    updating links to google

--- src/components/Info.js
+++ src/components/Info.js
@@ -5,9 +5,9 @@ export default function Info() {
         <section className="info">
             <h1 className="info--name">Lucy Macartney</h1>
             <h2 className="info--jobtitle">Software Developer</h2>
-            <a className="info--website" href="https://localhost:8080" target="_blank">lucywebsite.me</a>
+            <a className="info--website" href="https://google.com" target="_blank">lucywebsite.me</a>
             <div className="info--buttonscontainer">
-                <a href="#" target="_blank"><button className="info--button"><i className="fas fa-envelope"></i> Email</button></a>
+                <a href="https://google.com" target="_blank"><button className="info--button"><i className="fas fa-envelope"></i> Email</button></a>
                 <a href="https://linkedin.com/in/lucymacartney" target="_blank"><button className="info--button info--linkedin"><i className="fab fa-linkedin"></i> LinkedIn</button></a>
             </div>
         </section>
